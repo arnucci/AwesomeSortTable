45b70830811463a553d19658729b4bae8b360107
arnucci <arnaud.salvucci@gmail.com>
2013-07-20 17:30:52 +0200

    move demos

--- jquery.awesomeSortTable.js
+++ jquery.awesomeSortTable.js
@@ -8,18 +8,22 @@
  */
 (function ($) {
 
-    // Default settings
-    var defaultsSetting = {
-        all:    "Select all",
-        cancel: "Cancel",
-        ok:     "OK",
-        sortAZ: "Sort A-Z",
-        sortZA: "Sort Z-A"
-    },
-
-
     $.fn.awesomeSortTable=function(options) {
 
+        // Default settings
+        var defaultsSetting = {
+            all:    "Select all",
+            cancel: "Cancel",
+            ok:     "OK",
+            sortAZ: "Sort A-Z",
+            sortZA: "Sort Z-A"
+        };
+
+        var settings = $.extend(defaultsSetting, options);
+
+        return this.each(function() {
+            $(this).append('<span><img src="fleche.png" /></span>');
+        });
     };
 
 })(jQuery);
